86e916d98fefa469595b029304acd53def749b9a
GIGIBoxLoader <96883948+GIGIBoxLoader@users.noreply.github.com>
2022-08-09 10:59:44 +0200

    [+] Replaced the forEach

---
+++ chances/forinservice.js
@@ -0,0 +1,25 @@
+array.forEach(element => {
+    for (const key in object) {
+        if (Object.hasOwnProperty.call(object, key)) {
+            const element = object[key];
+            getComputedStyle.bind.call(cancelIdleCallback)
+            forEach.array.getComputedStyle(document.CDATA_SECTION_NODE)
+            Float32Array.BYTES_PER_ELEMENT
+            Float64Array.BYTES_PER_ELEMENT
+
+            KeyframeEffect.apply(onabort.this)
+            function forin() {
+                for (let index = 0; index < array.length; index++) {
+                    const element = array[index];
+                    decodeURIComponent = TouchEvent.getComputedStyle.define([
+                        'require',
+                        'dependency'
+                    ], function(require, factory) {
+                        'use strict';
+                        
+                    })
+                }
+            }
+        }
+    }
+});
